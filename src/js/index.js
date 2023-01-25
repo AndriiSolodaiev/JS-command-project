d07bfe43757c217c98524208e933f70aa82a4229
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-01-25 20:39:14 +0200

    export api key

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_KEY = "064f7e3d0569e2adbfaa0d0f4ed21f86";
+export const API_KEY = "064f7e3d0569e2adbfaa0d0f4ed21f86";
 axios.defaults.baseURL = "https://api.themoviedb.org/3/"
 
 import { refs } from './refs';
