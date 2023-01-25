e7f5dc08602c63ab09ec14f2c048c7b432a89a63
Miss-Anastasiia <110167985+Miss-Anastasiia@users.noreply.github.com>
2023-01-25 21:49:15 +0200

    картка

--- src/js/functions/card.js
+++ src/js/functions/card.js
@@ -1,43 +1,59 @@
+import { refs } from "../refs";
+
 export function createMoviesCardMarkup(movies) {
-  return movies
-    .map(
-      ({
-        original_title,
-        poster_path,
-        vote_average,
-        id,
-        genre_ids,
-        release_date,
-      }) => {
+  const {
+    original_title,
+    poster_path,
+    vote_average,
+    genre_ids,
+    release_date,
+  } = movies;
         let posterPath = ``;
         if (poster_path) {
           posterPath = `https://image.tmdb.org/t/p/w400/${poster_path}`;
         } else {
           posterPath =
             'https://cdn.create.vista.com/api/media/small/324908572/stock-vector-3d-cinema-film-strip-in';
-        }
-        return `<li class='films-gallery__item' key='${id}'>
+  }
+  let linea = '|'
+  if (genre_ids && release_date) {
+    linea = ''
+  }
+        return `
             <img
                 class='films-gallery__img'
                 src='${posterPath}'
                 alt='${original_title}'
-                width
                 loading='lazy'
             />
-            <span class='films-gallery__rate'>${vote_average.toFixed(1)}</span>
+            </span>
             <div class='films-gallery__wrap'>
                 <h2 class='films-gallery__title'>${original_title}</h2>
                 <div class='films-gallery__info'>
-                <p class='films-gallery__text'>${genre_ids}</p>
-                <p class='films-gallery__age'>| ${release_date}</p>
+                <p class='films-gallery__text'>${genre_ids}${linea}</p>
+                <p class='films-gallery__age'>${release_date}
+                <span class='films-gallery__rate'>${vote_average.toFixed(1)}</p>
                 </div>
-            </div>
-            </li>`;
-      }
-    )
-    .join('');
-}
-
+            </div>`;
+};
+      
+// export function setMovieGenresNames(array) {
+//   let genresCard = ''
+//    if (array.length > 2) {
+//      genresCard = 'Other';
+//    }
+//    else {
+//      genresCard = array.map(element => {
+//        console.log(element)
+//      console.log(refs.genresObj)
+//        return refs.genresObj[element]
+//       });
+    
+     
+//   }
+//   return genresCard;
+// }
+ 
 export function setMovieGenresNames(movies, genresList) {
   movies.forEach(movie => {
     movie.release_date = movie.release_date?.slice(0, 4);
