7a7adf51fcd394e57f5a30a6da9f338314b57686
Miss-Anastasiia <110167985+Miss-Anastasiia@users.noreply.github.com>
2023-01-26 22:26:24 +0200

    картка повністю готова

--- src/js/functions/card.js
+++ src/js/functions/card.js
@@ -1,6 +1,5 @@
-import { createGenresObj } from "./genres";
-
 export function createMoviesCardMarkup(movies) {
+
   const {
     title,
     original_title,
@@ -16,11 +15,14 @@ export function createMoviesCardMarkup(movies) {
           posterPath =
             'https://cdn.create.vista.com/api/media/small/324908572/stock-vector-3d-cinema-film-strip-in';
   }
-
-  let linea = ''
-  if (genre_ids && release_date) {
+  
+  let linea = '|'
+  if (genre_ids.length && release_date) {
     linea = '|'
+  } else {
+    linea = ''
   }
+
         return `
             <img
                 class='films-gallery__img'
@@ -32,50 +34,33 @@ export function createMoviesCardMarkup(movies) {
             <div class='films-gallery__wrap'>
                 <h2 class='films-gallery__title'>${title}</h2>
                 <div class='films-gallery__info'>
-                <p class='films-gallery__text'>${genre_ids.slice(0, 2).join(',')} ${linea} ${release_date.slice(0, 4,)}</p>
+                <p class='films-gallery__text'>${setMovieGenresNames(genre_ids)} ${linea} ${release_date.slice(0, 4,)}</p>
                 <p class='films-gallery__rate '>${vote_average.toFixed(1)}</p>
                 </div>
             </div>`;
   
-  
+   
             
 };
- 
 
-
-
-
-
-// export function setMovieGenresNames(array) {
-//   let genresCard = ''
-//    if (genresCard.length > 2) {
-//       genresCard.splice(2, genresCard.length - 1, 'Other');
-//    }
-//    else {
-//      genresCard = array.map(element => {
-//        console.log(element)
-//      console.log(refs.genresObj)
-//        return refs.genresObj[element]
-//       });
+function setMovieGenresNames(array) {
+const savedGenres = localStorage.getItem("genres");
+const parsedGenres = JSON.parse(savedGenres);
+  let genresCard = array.map(element => {
+   return parsedGenres[element]
+  })
+   if (genresCard.length > 2) {
+      genresCard.splice(2, genresCard.length - 1, 'Other');
+  }
+  if (!genresCard.length) {
     
-     
-//   }
-//   return genresCard;
-// }
+    return genresCard = ''
+  }
+  
+  return genresCard.join(', ');  
+  } 
+
  
-// export function setMovieGenresNames(movies, genresList) {
-//   movies.forEach(movie => {
-//     movie.release_date = movie.release_date?.slice(0, 4);
-//     const genresIdsList = movie.genre_ids;
-//     genresIdsList.forEach((genreId, index, array) => {
-//       const genresListItem = genresList.find(genre => genre.id === genreId);
-//       const idx = genresList.indexOf(genresListItem);
-//       array[index] = genresList[idx].name;
-//     });
-//     if (genresIdsList.length > 2) {
-//       genresIdsList.splice(2, genresIdsList.length - 1, 'Other');
-//     }
-//     movie.genre_ids = genresIdsList.join(', ');
-//   });
-// }
-// ;
+
+
+ 
