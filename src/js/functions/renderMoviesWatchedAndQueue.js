39883ebbeb1d87e0fa5237c4565c1eaae956c159
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2023-01-29 22:12:05 +0200

    add library message

--- src/js/functions/renderMoviesWatchedAndQueue.js
+++ src/js/functions/renderMoviesWatchedAndQueue.js
@@ -4,32 +4,34 @@ import { createMarkupMovies } from "./createMarkupMovies";
 export function renderMoviesWatchedAndQueue(storageKey, typeOfLibrary) {
     const moviesArr = JSON.parse(localStorage.getItem(storageKey));
 
-    // if(moviesArr.length === 0) {
-    //     refs.libraryContainerEl.insertAdjacentHTML('afterbegin', `
-    //     <hi class='film-colection__message'>
-    //         There's nothing here yet.
-    //     </hi>
-    // `)
-    // } else {
+    if(moviesArr.length === 0) {
+        refs.libraryMessageContainerEl.innerHTML = `
+            <p class='library-message__text'>
+                There's nothing here yet...
+            </p>
+        `
+        refs.moviesCollectionLibrary.innerHTML = '';
+    } else {
+        refs.libraryMessageContainerEl.innerHTML = '';
+
         const markup = createMarkupMovies(moviesArr);
-
         refs.moviesCollectionLibrary.innerHTML = markup;
-    // }
 
-    // page = moviesArr.slice(0, 20);
-    // на початку begin = 0, end = 20
-    // кожна наступна сторінка begin + 20, end + 20
+        // page = moviesArr.slice(0, 20);
+        // на початку begin = 0, end = 20
+        // кожна наступна сторінка begin + 20, end + 20
 
-    // if(!moviesArr) {
-    //     data.page = null;
-    // } else {
-    //     data.page = page;
-    // }
-    
-    data.totalPage = Math.ceil(moviesArr.length / 20);
-    data.typePagination = typeOfLibrary;
+        // if(!moviesArr) {
+        //     data.page = null;
+        // } else {
+        //     data.page = page;
+        // }
+        
+        data.totalPage = Math.ceil(moviesArr.length / 20);
+        data.typePagination = typeOfLibrary;
 
-    // markupPagination(); 
+        // markupPagination(); 
+    }
 }
 
 //storageKey - 'watchedMovies' or 'queueMovies'
--- src/js/refs.js
+++ src/js/refs.js
@@ -5,10 +5,11 @@ export const refs = {
   openModal: document.querySelector("[data-modal-open]"),
   form: document.querySelector('.js-search-form'),
   input: document.querySelector('.js-search-input'),
-  moviesCollectionLibrary: document.querySelector('.js-library-film-colection__list'),
+  moviesCollectionLibrary: document.querySelector('.js-library-films__list'),
   openWatchedBtnEl: document.querySelector('.button__watched'),
   openQueueBtnEl: document.querySelector('.button__queue'),
   libraryContainerEl: document.querySelector('.js-library-container'),
+  libraryMessageContainerEl: document.querySelector('.library-message'),
 };
 
 //масиви з об'єктами фільмів my library, що будуть збережені до localStorage
