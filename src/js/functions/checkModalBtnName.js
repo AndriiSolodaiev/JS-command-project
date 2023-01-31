ed9382e6b36fd9e4adf4435eb76d40d0f0e2fbd5
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2023-01-31 22:30:44 +0200

    add modal buttons style

--- src/js/functions/checkModalBtnName.js
+++ src/js/functions/checkModalBtnName.js
@@ -4,8 +4,14 @@ export function checkModalBtnName(currentMovie, btnEl, action, storageKey) {
     if(storageArr) {
         if(storageArr.find(libraryMovie => currentMovie.id === libraryMovie.id)) {
             btnEl.textContent = `Remove from ${action}`;
+
+            btnEl.classList.add('mw-movie__current-btn');
         } else {
-            btnEl.textContent = `Add to ${action}`;
+            btnEl.textContent = `Add to ${action}`;     
+
+            if(btnEl.classList.contains('mw-movie__current-btn')) {
+                btnEl.classList.remove('mw-movie__current-btn');
+            }
         }
     }
 }
